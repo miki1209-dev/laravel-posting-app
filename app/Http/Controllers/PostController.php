d4f7705ee88b8cd35e4a12d5b27786cf14b6ba7f
miki1209-dev <crest0421@gmail.com>
2025-04-08 14:20:18 +0900

    自分の投稿したデータのみ取得に変更

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,11 +12,11 @@ class PostController extends Controller
 	// 投稿一覧ページ
 	public function index()
 	{
-		// $posts = Auth::user()->posts()->orderBy('updated_at', 'asc')->get();
+		$posts = Auth::user()->posts()->orderBy('updated_at', 'asc')->get();
 		// dump($posts);
 		$username = Auth::user()->name;
 		// dump($username);
-		$posts = Post::orderBy('created_at', 'desc')->get();
+		// $posts = Post::orderBy('created_at', 'desc')->get();
 		return view('posts.index', compact('posts', 'username'));
 	}
 
